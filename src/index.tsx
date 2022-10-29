fa241605bf813eb9c3cc5b81036d14aba3b7c561
[user_name] <[user_email_address]>
2022-10-29 15:27:46 +0900

    vueみたいに普通にコンポーネントを作って呼び出すだけ

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,25 @@
 import * as React from "react";
 import {render} from "react-dom";
 import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+import { Editor } from './pages/editor';
+
+const Globalstyle = createGlobalStyle`
+  body * { 
+    box-sizing: border-box;
+    }
+`
+
+const Main = (
+<>
+  <Globalstyle/>
+  <Editor/>
+</>)
+render(Main,document.getElementById("app"))
 
 // スタイル付きのコンポーネントを作成
-const Header = styled.h1`
-color: red;
-`
+// const Header = styled.h1`
+// color: red;
+// `
 // 文字を埋め込む
-const Main = (<Header>neko</Header>)
-
-render(Main,document.getElementById("app"))
+// const Main = (<Header>neko</Header>)
