13f25fa1602e700bedb5049fb003208ac2157e26
[user_name] <[user_email_address]>
2022-10-29 10:58:18 +0900

    pagesから閲覧できるようにする

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import {render} from "react-dom";
+
+const Main = (<h1>neko</h1>)
+
+render(Main,document.getElementById("app"))
--- webpack.config.js
+++ webpack.config.js
@@ -16,3 +16,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+        publicPath: 'dist/',
+    }
+}
